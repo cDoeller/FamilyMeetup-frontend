d9d7e4c9c0ea0bfc08a0eb732a269062fbdf8c1e
Christian Doeller <mail@christiandoeller.de>
2024-02-15 20:10:45 +0100

    added about component and  basic structure and styling for homepage

---
+++ src/components/HeaderAbout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HeaderAbout() {
+  return (
+    <div className="header-about-homepage-wrapper">
+      <h1 className="header-about-title">
+        Did you ever wonder how to meet other people while being busy with a
+        full-time job, household, ... your family?
+      </h1>
+      <h3 className="header-about-subtitle">
+        With familyMeetup you can find events in your area that are child and
+        family friendly. The best thing is that you can be sure to be there with
+        like minded people - connect and have fun ☘️
+      </h3>
+    </div>
+  );
+}
+
+export default HeaderAbout;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import HeaderAbout from "../components/HeaderAbout";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  const [locationFilter, setLocationFilter] = useState("");
+
+  return (
+    <div>
+      <HeaderAbout />
+      <Link to="/events">
+        <button className="all-events-button">show all events</button>{" "}
+      </Link>
+      <div className="random-filter-events-wrapper">
+        <label htmlFor="random-input">
+          random events in
+          <input
+            className="random-filter-events-input"
+            name="random-input"
+            type="text"
+            value={locationFilter}
+            onChange={(e) => {
+              setLocationFilter(e.target.value);
+            }}
+          />
+        </label>
+      </div>
+      <div className="events-list-container">
+        {/* MAP HERE AS SOON AS DATA AVAILABLE */}
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
