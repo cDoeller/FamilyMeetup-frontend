362bb780f94359f2ff851256c4bff3d450bf2650
Christian Doeller <mail@christiandoeller.de>
2024-02-15 21:06:06 +0100

    event cards, nav styling, hp structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import HeaderAbout from "../components/HeaderAbout";
 
 function HomePage() {
+  const [randomEvents, setRandomEvents] = useState(null);
   const [locationFilter, setLocationFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5005/events")
+      .then((response) => {
+        console.log(response.data);
+        setRandomEvents(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
@@ -27,7 +41,25 @@ function HomePage() {
         </label>
       </div>
       <div className="events-list-container">
-        {/* MAP HERE AS SOON AS DATA AVAILABLE */}
+        {randomEvents &&
+          randomEvents.map((event) => {
+            return (
+              <Link key={event.id} className="event-card">
+                <div className="event-card-image-wrapper">
+                  <img
+                    src={event.image_url}
+                    alt=""
+                    className="event-card-image"
+                  />
+                </div>
+                <h3 className="event-card-title">{event.title}</h3>
+                <h3 className="event-card-datetime">
+                  {event.time + " " + event.date}
+                </h3>
+                <p className="event-card-description">{event.description}</p>
+              </Link>
+            );
+          })}
       </div>
     </div>
   );
