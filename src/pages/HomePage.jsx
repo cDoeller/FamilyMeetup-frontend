04e160a8fd8cf8415724a3c7881dd37db30705b3
Christian Doeller <mail@christiandoeller.de>
2024-02-17 13:01:08 +0100

    added random helper function, working on filtering

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,30 +6,58 @@ import HeaderAbout from "../components/HeaderAbout";
 import EventCard from "../components/EventCard";
 
 function HomePage() {
-  const [randomEvents, setRandomEvents] = useState(null);
+  const [allEvents, setAllEvents] = useState(null);
   const [locationFilter, setLocationFilter] = useState("");
+  const [location, setLocation] = useState(true);
+
 
   // ** render only 6 random cards to the screen **
   useEffect(() => {
     axios
-      .get("http://localhost:5005/events")
+      .get(`http://localhost:5005/events`)
       .then((response) => {
         console.log(response.data);
-        setRandomEvents(response.data);
+        setAllEvents(response.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  // ** filtering **
-  useEffect(() => {}, [locationFilter]);
+  // get *amount* of random events
+  function getRandomEvents(amount, array) {
+    // only do this if we fetched all events in state
+    const maxIndex = allEvents.length;
+    // getting *amount* individual random index nums in array
+    let randIndexVals = [];
+    while (randIndexVals.length < amount) {
+      let randNum = Math.floor(Math.random() * (maxIndex));
+      if (!randIndexVals.includes(randNum)) randIndexVals.push(randNum);
+    }
+    console.log(randIndexVals);
+    // pick objects from all events based on these index nums
+    let randomEventsTemp = [];
+    for (let i = 0; i < randIndexVals.length; i++) {
+      randomEventsTemp.push(allEvents[randIndexVals[i]]);
+    }
+    console.log(randomEventsTemp);
+    return randomEventsTemp
+  }
+
+  let filtered = []
+  if (allEvents) {
+    if (location) {
+      // filtered = allEvents.filter
+    } else {
+      filtered = getRandomEvents(4)
+    }
+  }
 
   return (
     <div className="page-wrapper">
       <HeaderAbout />
       <Link to="/events">
-          <button className="all-events-button">show all events</button>
+        <button className="all-events-button">show all events</button>
       </Link>
       <div className="homepage-random-events-wrapper">
         <div className="random-filter-events-wrapper">
@@ -47,8 +75,8 @@ function HomePage() {
           />
         </div>
         <div className="events-list-container">
-          {randomEvents &&
-            randomEvents.map((event) => {
+          {allEvents &&
+            filtered.map((event) => {
               return (
                 <Link
                   to={`/events/${event.id}`}
