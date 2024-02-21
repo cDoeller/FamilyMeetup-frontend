ca03c2b09cf69eb1c851e9eb48d28a1765d7b99f
Natalia Pinto <pinto.natalia@outlook.com>
2024-02-21 18:39:21 +0100

    delete and edit button links added

--- src/pages/EventsDetailAdmin.jsx
+++ src/pages/EventsDetailAdmin.jsx
@@ -1,11 +1,86 @@
 import React from 'react'
-
+import "../styles/EventDetails.css"
+import {Link, useParams} from "react-router-dom"
+import {useState, useEffect} from "react"
+import axios from "axios"
 function EventsDetailAdmin() {
-  return (
-    <div>
-      Admin Route
-    </div>
-  )
+    const [event, setEvent] = useState(null);
+    const { eventId } = useParams();
+    const [count, setCount] = useState(0);
+  
+    function increaseCount() {
+      setCount((prevCount) => {
+        return prevCount + 1;
+      });
+    }
+    useEffect(() => {
+      axios
+        .get(`http://localhost:5005/events/${eventId}`)
+        .then((response) => {
+          setEvent(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, [eventId]);
+  
+    if (!event)
+      return (
+        <div>
+          <p className="loading">loading..</p>
+        </div>
+      );
+    if (event)
+      return (
+        <div className="page-wrapper">
+          <div className="one-event">
+            <div className="event-image">
+              <img src={event.image_url} alt="" />
+            </div>
+            <div className="event-info">
+              <div className="event-title-category-wrapper">
+                <div className="event-title-short-description-wrapper">
+                  <h1 className="event-title">{event.title}</h1>
+                  <h3 className="event-short-description">
+                    {event.short_description}
+                  </h3>
+                </div>
+                <h3 className="event-category">{event.category}</h3>
+              </div>
+              <p className="event-description">{event.description}</p>
+              <div className="event-date-time-price-wrapper">
+                <div className="event-date-time">
+                  <h3 className="event-date">{event.date}</h3>
+                  <h3 className="event-time">{event.time}</h3>
+                </div>
+                <h3 className="event-price">{event.price}€</h3>
+              </div>
+              <h3 className="event-location">{event.location}</h3>
+            </div>
+            <div className="event-participants-wrapper">
+              <div className="event-families-going-join-wrapper">
+                <p className="event-families-going">
+                  {event.participants + count} families are going
+                </p>
+                <button onClick={increaseCount} className="event-join-button">
+                  join event
+                </button>
+              </div>
+              <div className="event-back-button-wrapper">
+                <Link to={`/events`}>
+                  <button className="event-back-button">Back</button>
+                </Link>
+                <Link to={`/admin/:eventId/edit`}>
+                  <button className="event-edit-button">Edit</button>
+                </Link>
+                <Link to={`/admin`}>
+                  <button className="event-delete-button">Delete</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
 }
 
 export default EventsDetailAdmin
