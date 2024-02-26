e69cb1ce862eee3e396bfd3076d2d60d8bfd3fa7
Christian Doeller <mail@christiandoeller.de>
2024-02-26 16:20:43 +0100

    past events basic styling

--- src/pages/PastEvents.jsx
+++ src/pages/PastEvents.jsx
@@ -23,36 +23,49 @@ function PastEvents() {
 
   return (
     <div className="page-wrapper">
-      {pastEvents &&
-        pastEvents.map((event) => {
-          return (
-            <div key={event.id} className="past-event-wrapper">
-              <div className="past-event-card-wrapper">
-                <div className="past-event-image-wrapper">
-                  <img src={event.image_url} />
+      <div className="past-event-all-wrapper">
+        {pastEvents &&
+          pastEvents.map((event) => {
+            return (
+              <div key={event.id} className="past-event-single-wrapper">
+                <div className="past-event-card-wrapper">
+                  <div className="past-event-image-wrapper">
+                    <img src={event.image_url} />
+                  </div>
+                  <div className="past-event-info-wrapper">
+                    <h3>{event.title}</h3>
+                    <h3>{event.date}</h3>
+                    <h3>{event.stories.length} Stories</h3>
+                  </div>
+                  <div className="past-event-create-button-wrapper">
+                    <button className="past-event-create-button">
+                      create story
+                    </button>
+                  </div>
                 </div>
-                <div className="past-event-info-wrapper">
-                  <h3>{event.title}</h3>
-                  <h3>{event.date}</h3>
-                  <h3>{event.stories.length} Stories</h3>
-                </div>
-                <button className="past-event-create-button">create story</button>
+                {event.stories.length > 0 &&
+                  event.stories.map((story) => {
+                    return (
+                      <div
+                        key={story.eventId + story.user_name}
+                        className="past-event-story-wrapper"
+                      >
+                        <div className="past-event-story-placeholder"></div>
+                        <div className="past-event-story-content">
+                          <h3 className="past-event-story-username">
+                            {story.user_name}
+                          </h3>
+                          <p className="past-event-story-text">
+                            {story.story_text}
+                          </p>
+                        </div>
+                      </div>
+                    );
+                  })}
               </div>
-              {event.stories.length > 0 &&
-                event.stories.map((story) => {
-                  return (
-                    <div
-                      key={story.eventId+story.user_name}
-                      className="past-event-story-wrapper"
-                    >
-                      <h3 className="past-event-story-username">{story.user_name}</h3>
-                      <p className="past-event-story-text">{story.story_text}</p>
-                    </div>
-                  );
-                })}
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
     </div>
   );
 }
