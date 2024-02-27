a3237383ada664270c957857549167f9c5e2a982
Natalia Pinto <pinto.natalia@outlook.com>
2024-02-27 20:54:40 +0100

    Popular events sorting included in homepage with new component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import HeaderAbout from "../components/HeaderAbout";
 import EventCard from "../components/EventCard";
 import "../styles/HomePage.css";
+import PopularEvents from "../components/PopularEvents";
 
 // not sure here -->
 let generatedRandomBaseEvents = false;
@@ -115,6 +116,7 @@ function HomePage() {
             })}
         </div>
       </div>
+      <PopularEvents></PopularEvents>
     </div>
   );
 }
